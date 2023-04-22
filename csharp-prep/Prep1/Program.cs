f84f7fe49f1ce6c1a2d9cfa635cc29933fdb834b
mosiahvb <mosiahvaldezbates@gmail.com>
2023-04-22 16:44:54 -0600

    edit

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //ask user for name
         Console.Write("what is your first name? ");
         string first = Console.ReadLine();
         Console.Write("what is your last name? ");
