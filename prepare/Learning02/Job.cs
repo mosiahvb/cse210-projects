c4a37e8e652cdd1e5117d07cd1210f9ef96736f8
mosiahvb <mosiahvaldezbates@gmail.com>
2023-04-26 14:55:17 -0600

    testing

---
+++ prepare/Learning02/Job.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Job
+
+{
+    public string _JobTitle;
+    public string _Company;
+    public int _StartYear;
+    public int _EndYear;
+
+    public void Display()
+    {
+        Console.WriteLine($"{_JobTitle} ({_Company}){_StartYear}-{_EndYear} ");
+    }
+}
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 =new Job();
+        job1._JobTitle = "Project manager";
+        job1._Company = "U.S. Air Force";
+        job1._StartYear = 2012;
+        job1._EndYear = 2017;
+
+        Job job2 = new Job();
+        job2._JobTitle = "Analyst";
+        job2._Company = "CIA";
+        job2._StartYear= 2018;
+        job2._EndYear= 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Mosiah Valdez";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
