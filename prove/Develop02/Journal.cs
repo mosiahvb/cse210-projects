f17d17192d01bda51821ba3fc31cd9cf4bda1e3c
mosiahvb <mosiahvaldezbates@gmail.com>
2023-04-27 22:51:40 -0600

    changes

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,33 @@
+
+
+class Journal
+{
+  List<string> entries;
+
+
+  public Journal()
+  {
+
+  }
+    
+  public void DisplayJournalEntries()
+  {
+
+  }
+
+  public void CreateJournalEntry()
+  {
+
+  }
+
+  public void SaveToCSV()
+  {
+
+  }
+
+  public void LoadFromCSV()
+  {
+
+  }
+  
+}
---
+++ prove/Develop02/PromptGenerator.cs
@@ -0,0 +1,15 @@
+class PromptGenerator
+{
+    List<string> prompts;
+
+    public PromptGenerator()
+    {
+        prompts= new List<string>();
+
+    }
+
+    public string GetRandomPrompt()
+    {
+        return "";
+    }
+}
