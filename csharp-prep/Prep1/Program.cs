8f5dbd8c8905c3c550b42f3eb893f7c274cc3ce8
mosiahvb <mosiahvaldezbates@gmail.com>
2023-06-09 00:53:02 -0600

    done

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -8,7 +8,7 @@ class Program
         Console.Write("What is your first name? ");
         string firstname = Console.ReadLine();
 
-        Console.Write("what is your last name ");
+        Console.Write("what is your last name? ");
         string lastname = Console.ReadLine();
 
         Console.WriteLine($"my name is {lastname}, {firstname} {lastname}");
